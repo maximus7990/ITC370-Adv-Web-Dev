24cdab8e342c519e72d5cede92b5aa47cba494f8
maximus7990 <bsb8s@missouristate.edu>
2021-10-26 10:02:20 -0500

    main

---
+++ Java Intro/scripts.js
@@ -0,0 +1,2 @@
+// document.write("<footer class='bg-dark text-light'>Thank you again</footer>");
+document.getElementById("theFooter").innerHTML = "<footer class='bg-dark text-light'>Thank you again</footer>";
